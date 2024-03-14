c36f6649aea3aa2a24e4af7b42bbe2d832f2f716
ivo aragao <ivoaragao4@gmail.com>
2024-03-14 09:41:54 -0300

    fix: mudar url do projeto do supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 //supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hbgouhetuiojmlgllvni.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiZ291aGV0dWlvam1sZ2xsdm5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNjQ0NTYsImV4cCI6MjAyMjg0MDQ1Nn0.3KbJjdgmaDG_WTBxW3YxiT1LhyCYO9ar_yv0XppKuuE'
+const supabaseUrl = 'https://ytbyyqxxlpocqdbqijnm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0Ynl5cXh4bHBvY3FkYnFpam5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwOTY3OTIsImV4cCI6MjAyMzY3Mjc5Mn0.ZN4PDFtmynBqzVrWJeO-QGGewq_2rLZMOOc_EEFOMbY'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
